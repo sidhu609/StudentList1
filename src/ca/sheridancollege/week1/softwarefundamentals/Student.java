3122eb43f100ffd6a9395999c59edf56cdc62031
Ramandeep Sidhu <sidhu609@sheridancollege.ca>
2020-06-07 20:04:34 -0400

    Reverted Commit #2

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -13,23 +13,20 @@ public class Student {
  
        private String name;
 
-    
-    private String email;
-     
-    public String getEmail() {
-        return email;
-    }
-
-  
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
     public void setName(String name) {
         this.name = name;
     }
